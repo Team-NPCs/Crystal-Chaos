792bcd1629e7370ddc30a2e66b3326e8f0366088
Ca3rwin <ivan.valluzzi@gmail.com>
2023-06-13 20:55:03 +0200

    fixing networking and demomanager

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour {
+    private bool isAlreadyActive;
     [SerializeField] private Button hostBtn;
+    [SerializeField] private GameObject _demoManager;
+    [SerializeField] private GameObject _hud;
     [SerializeField] private Button clientBtn;
 
     private void Awake() {
         hostBtn.onClick.AddListener(() => {
+            // Loader.Load(Loader.Scene.MainStage);
             Debug.Log("Starting Host");
             NetworkManager.Singleton.StartHost();
-        });
 
+            _demoManager.SetActive(true);
+            _hud.SetActive(true);
+            gameObject.SetActive(false);
+        });
         clientBtn.onClick.AddListener(() => {
+            // Loader.Load(Loader.Scene.MainStage);
             Debug.Log("Starting Client");
             NetworkManager.Singleton.StartClient();
         });
     }
+    // private void Update() {
+    //     if (true  /*when client connects && isAlreadyActive*/) {
+    //         _demoManager.SetActive(true);
+    //         _hud.SetActive(true);
+    //         gameObject.SetActive(false);
+    //         isAlreadyActive = true;
+    //     }
+    // }
 }
