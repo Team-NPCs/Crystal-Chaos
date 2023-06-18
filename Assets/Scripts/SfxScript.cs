6c2c84a22efeba858eb04f8d12f8bb90c50ff1fb
Ca3rwin <ivan.valluzzi@gmail.com>
2023-06-19 01:38:47 +0200

    Added remaining spell sounds

--- Assets/Scripts/SfxScript.cs
+++ Assets/Scripts/SfxScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SfxScript : MonoBehaviour {
     [SerializeField] private AudioSource earthAudio, airAudio, voidAudio, waterAudio, fireAudio;
     [SerializeField] private AudioSource healthPotionAudio, movementPotionAudio;
+    public AudioSource deathAudio;
     public readonly Dictionary<CrystalType, AudioSource> spellAudio = new();
     public readonly Dictionary<PotionType, AudioSource> potionAudio = new();
 
